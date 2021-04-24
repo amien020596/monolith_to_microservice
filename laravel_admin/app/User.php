375268fcf9467a23765c19c25ead8eac310d95dd
amien kurniawan <offdevamienk@gmail.com>
2021-04-25 00:00:10 +0700

    update laravel passport

--- laravel_admin/app/User.php
+++ laravel_admin/app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\HasApiTokens;
 
 /**
  * App\User
@@ -33,7 +34,7 @@ use Illuminate\Support\Facades\Hash;
  */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
