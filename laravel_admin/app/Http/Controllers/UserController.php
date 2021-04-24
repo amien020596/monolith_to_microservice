284df4254d5c24d34135659001279ddad2757769
amien kurniawan <offdevamienk@gmail.com>
2021-04-24 19:37:52 +0700

    update REST method

--- laravel_admin/app/Http/Controllers/UserController.php
+++ laravel_admin/app/Http/Controllers/UserController.php
@@ -4,11 +4,36 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
     public function index()
     {
         return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return response($user, Response::HTTP_ACCEPTED);
     }
 }
