b7677d2f1bc8451ad583b3825cdecc8a98de97a5
amien kurniawan <offdevamienk@gmail.com>
2021-05-09 20:43:59 +0700

    update orders

---
+++ laravel_admin/database/seeds/OrderSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Order;
+use App\OrderItem;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Order::class, 30)->create()->each(function ($order) {
+            factory(OrderItem::class, random_int(1, 5))->create([
+                'order_id' => $order->id
+            ]);
+        });
+    }
+}
