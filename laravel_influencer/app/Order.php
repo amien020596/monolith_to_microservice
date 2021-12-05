999594e167cd1d0d5b373c73fff7cf6a194a27a2
amien kurniawan <offdevamienk@gmail.com>
2021-12-05 11:36:49 +0700

    Section 4 Sending emails

--- laravel_influencer/app/Order.php
+++ laravel_influencer/app/Order.php
@@ -27,6 +27,26 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereLastName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
  * @mixin \Eloquent
+ * @property string $code
+ * @property string $user_id
+ * @property string $influencer_email
+ * @property string|null $address
+ * @property string|null $address2
+ * @property string|null $city
+ * @property string|null $country
+ * @property string|null $zip
+ * @property int $complete
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAddress2($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereComplete($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereInfluencerEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereZip($value)
+ * @property string|null $transaction_id
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereTransactionId($value)
  */
 class Order extends Model
 {
@@ -35,12 +55,27 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
+    public function getInfluencerTotalAttribute()
+    {
+        return $this->orderItems->sum(function (OrderItem $item) {
+            return $item->influencer_revenue * $item->quantity;
+        });
+    }
+
+    public function getAdminTotalAttribute()
+    {
+        return $this->orderItems->sum(function (OrderItem $item) {
+            return $item->admin_revenue * $item->quantity;
+        });
+    }
+
     public function getTotalAttribute()
     {
         return $this->orderItems->sum(function (OrderItem $item) {
             return $item->price * $item->quantity;
         });
     }
+
     public function getNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
