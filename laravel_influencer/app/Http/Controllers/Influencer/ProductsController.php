7bfe376479f9f3ffb4752742e09451f69830ea69
amien kurniawan <offdevamienk@gmail.com>
2021-12-06 23:03:19 +0700

    update (filter cached)

--- laravel_influencer/app/Http/Controllers/Influencer/ProductsController.php
+++ laravel_influencer/app/Http/Controllers/Influencer/ProductsController.php
@@ -6,20 +6,23 @@ use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class ProductsController
 {
     public function index(Request $request)
     {
 
-        return Cache::remember('products', 5, function () use ($request) {
+        $products = Cache::remember('products', 5, function () use ($request) {
             sleep(10);
-            $query = Product::query();
-            if ($s = $request->input('s')) {
-                $query->whereRaw("title LIKE '%{$s}%'")
-                    ->orWhereRaw("description LIKE '%{$s}%'");
-            }
-            return ProductResource::collection($query->get());
+            return  Product::get();
         });
+
+        if ($s = $request->input('s')) {
+            $products = $products->filter(function (Product $product) use ($s) {
+                return Str::contains($product->title, $s) || Str::contains($product->description, $s);
+            });
+        }
+        return ProductResource::collection($products);
     }
 }
